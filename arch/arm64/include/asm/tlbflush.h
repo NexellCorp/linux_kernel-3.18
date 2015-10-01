94ba13fa8b8c52b17d5d9f1c8e60485db02ba282
jhkim <jhkim@nexell.co.kr>
2015-10-01 13:50:47 +0900

    BUG: tlb flush bug in android -> each tlb flush function set tlb all flush function

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -80,27 +80,38 @@ static inline void flush_tlb_all(void)
 
 static inline void flush_tlb_mm(struct mm_struct *mm)
 {
+#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+	flush_tlb_all();
+#else
 	unsigned long asid = (unsigned long)ASID(mm) << 48;
 
 	dsb(ishst);
 	asm("tlbi	aside1is, %0" : : "r" (asid));
 	dsb(ish);
+#endif
 }
 
 static inline void flush_tlb_page(struct vm_area_struct *vma,
 				  unsigned long uaddr)
 {
+#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+	flush_tlb_all();
+#else
 	unsigned long addr = uaddr >> 12 |
 		((unsigned long)ASID(vma->vm_mm) << 48);
 
 	dsb(ishst);
 	asm("tlbi	vae1is, %0" : : "r" (addr));
 	dsb(ish);
+#endif
 }
 
 static inline void __flush_tlb_range(struct vm_area_struct *vma,
 				     unsigned long start, unsigned long end)
 {
+#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+	flush_tlb_all();
+#else
 	unsigned long asid = (unsigned long)ASID(vma->vm_mm) << 48;
 	unsigned long addr;
 	start = asid | (start >> 12);
@@ -110,10 +121,14 @@ static inline void __flush_tlb_range(struct vm_area_struct *vma,
 	for (addr = start; addr < end; addr += 1 << (PAGE_SHIFT - 12))
 		asm("tlbi vae1is, %0" : : "r"(addr));
 	dsb(ish);
+#endif
 }
 
 static inline void __flush_tlb_kernel_range(unsigned long start, unsigned long end)
 {
+#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+	flush_tlb_all();
+#else
 	unsigned long addr;
 	start >>= 12;
 	end >>= 12;
@@ -123,6 +138,7 @@ static inline void __flush_tlb_kernel_range(unsigned long start, unsigned long e
 		asm("tlbi vaae1is, %0" : : "r"(addr));
 	dsb(ish);
 	isb();
+#endif
 }
 
 /*
@@ -155,11 +171,15 @@ static inline void flush_tlb_kernel_range(unsigned long start, unsigned long end
 static inline void __flush_tlb_pgtable(struct mm_struct *mm,
 				       unsigned long uaddr)
 {
+#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+	flush_tlb_all();
+#else
 	unsigned long addr = uaddr >> 12 | ((unsigned long)ASID(mm) << 48);
 
 	dsb(ishst);
 	asm("tlbi	vae1is, %0" : : "r" (addr));
 	dsb(ish);
+#endif
 }
 /*
  * On AArch64, the cache coherency is handled via the set_pte_at() function.
--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -50,7 +50,7 @@
 #include <asm/mmu_context.h>
 #include <asm/processor.h>
 #include <asm/stacktrace.h>
-#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+#if defined (CONFIG_SMP) && defined (__CONFIG_ARCH_S5P6818__)
 #include <asm/tlbflush.h>
 #endif
 
@@ -382,7 +382,7 @@ static void tls_thread_switch(struct task_struct *next)
 	: : "r" (tpidr), "r" (tpidrro));
 }
 
-#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+#if defined (CONFIG_SMP) && defined (__CONFIG_ARCH_S5P6818__)
 static void tlb_flush_thread(struct task_struct *prev)
 {
 	/* Flush the prev task's TLB entries */
@@ -399,7 +399,7 @@ struct task_struct *__switch_to(struct task_struct *prev,
 {
 	struct task_struct *last;
 
-#if defined (CONFIG_SMP) && defined (CONFIG_ARCH_S5P6818)
+#if defined (CONFIG_SMP) && defined (__CONFIG_ARCH_S5P6818__)
 	tlb_flush_thread(prev);
 #endif
 
